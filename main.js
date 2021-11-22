bd28ebdec254ff2f6793c5296fdde0785330884c
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-11-22 14:30:32 +0100

    add script to close hamburger menu

--- main.js
+++ main.js
@@ -6,8 +6,19 @@ mostrare / nascondere il menu principale
 const elementHamburger = document.querySelector('.fa-bars');
 console.log(elementHamburger);
 
+// creo variabile per il div hamburger menu
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
 // al click dell'icona, l'hamburger menu diventa display block
 elementHamburger.addEventListener('click', function () {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
     hamburgerMenu.style.display = 'block';
+})
+
+//creo variabile per icona chiusura hamburger menu
+const elementClose = document.querySelector('.fa-times');
+console.log(elementClose);
+
+//al click dell'icona, l'hamburger menu diventa display none
+elementClose.addEventListener('click', function () {
+    hamburgerMenu.style.display = 'none';
 })
