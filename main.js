eaf1db23cf43779a73c3eba6e1f6e2a7e6029ee6
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-11-22 17:28:07 +0100

    add alternative script

--- main.js
+++ main.js
@@ -22,3 +22,25 @@ console.log(elementClose);
 elementClose.addEventListener('click', function () {
     hamburgerMenu.style.display = 'none';
 })
+
+//Alternativamente utilizzo la classe .active:
+// // creo variabile per icona hamburger menu
+// const elementHamburger = document.querySelector('.fa-bars');
+// console.log(elementHamburger);
+
+// // creo variabile per il div hamburger menu
+// const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+// // al click dell'icona, l'hamburger menu diventa display block
+// elementHamburger.addEventListener('click', function () {
+//     hamburgerMenu.classList.add('active');
+// })
+
+// //creo variabile per icona chiusura hamburger menu
+// const elementClose = document.querySelector('.fa-times');
+// console.log(elementClose);
+
+// //al click dell'icona, l'hamburger menu diventa display none
+// elementClose.addEventListener('click', function () {
+//     hamburgerMenu.classList.remove('active');
+// })
