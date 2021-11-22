bc293302b0eba9ea3428efc50dbb860c2798296a
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-11-22 14:25:06 +0100

    add script to show hamburger menu

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 /* Hamburger menu:
 mostrare / nascondere il menu principale
 (per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile). */
+
+// creo variabile per icona hamburger menu
+const elementHamburger = document.querySelector('.fa-bars');
+console.log(elementHamburger);
+
+// al click dell'icona, l'hamburger menu diventa display block
+elementHamburger.addEventListener('click', function () {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    hamburgerMenu.style.display = 'block';
+})
